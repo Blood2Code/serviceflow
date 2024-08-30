e185a24b6da744cec6b66c250e3f8869f6a54629
Umar <abduxalilraxmonberdiyev@gmail.com>
2024-08-30 18:22:35 +0500

    init:

---
+++ src/test/java/org/example/serviceflow/ServiceflowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.serviceflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

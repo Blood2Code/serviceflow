ccb403a946d181e48311f3150c1394b0fd1c7a92
Umar <abduxalilraxmonberdiyev@gmail.com>
2024-09-19 08:26:57 +0500

    add: response

---
+++ src/main/java/uz/devid/serviceflow/entity/Response.java
@@ -0,0 +1,45 @@
+package uz.devid.serviceflow.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @UuidGenerator
+    private UUID UUID;
+
+    @Column(name = "source_service", nullable = false)
+    private String sourceService;
+
+    @Column(name = "destination_service", nullable = false)
+    private String destinationService;
+
+    @Column(name = "request_path", nullable = false)
+    private String requestPath;
+
+    @Column(name = "domain_path", nullable = false)
+    private String domainPath;
+
+    @Column(name = "http_method", nullable = false)
+    private String httpMethod;
+
+    @Column(name = "status_code")
+    private Integer statusCode;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+    @Column(name = "response_payload", columnDefinition = "TEXT")
+    private String responsePayload;
+}
